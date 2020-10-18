5da600b0bf78cbc52b422d5807154a7cce2fa79d
MichaelT <mtarquini19@gmail.com>
2020-10-17 19:07:18 -0700

    added current date to header

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 // Step 1: use moment to display current date & time
+// create variables
 
+var time = moment();
+var date = time.format("MMM DD YYYY");
+console.log(time);
+$("#currentDay").text("Today is " + date);
 
 // Step 2: display past, current, and upcoming times in different colors
 
