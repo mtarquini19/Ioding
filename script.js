a42f27bbc5104f9bae143738c51b0387685fbb7a
MichaelT <mtarquini19@gmail.com>
2020-10-17 17:34:52 -0700

    outline timeblocks in html, rough psuedocode for js

---
+++ script.js
@@ -0,0 +1,10 @@
+// Step 1: use moment to display current date & time
+
+
+// Step 2: display past, current, and upcoming times in different colors
+
+
+// Step 3: capture user input and save to local storage
+
+
+// Step 4: loop through local storage to refresh page with current events
